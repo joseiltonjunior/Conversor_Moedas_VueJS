1b4daa55de4afba6bf1f723ec278578f630e5193
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-09-23 17:01:59 -0300

    Conversor de Moedas com Vue
    projeto de um conversor de moeda usando o Vue.JS

---
+++ src/components/Conversor.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="conversor">
+    <h2>{{moedaA}} para {{moedaB}}</h2>
+    <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA" />
+    <input type="button" value="Converter" v-on:click="converter" />
+    <h2>{{moedaB_value}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Conversor",
+  props: ["moedaA", "moedaB"],
+  data() {
+    return {
+      moedaA_value: "",
+      moedaB_value: 0
+    };
+  },
+  methods: {
+    converter() {
+      let de_para = this.moedaA + "_" + this.moedaB;
+
+      let url =
+        "https://free.currencyconverterapi.com/api/v5/convert?q=" +
+        de_para +
+        "&compact=y&apiKey=8244817c1442e2442477";
+
+      fetch(url)
+        .then(res => {
+          return res.json();
+        })
+        .then(json => {
+          let cotacao = json[de_para].val;
+          this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(
+            2
+          );
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
